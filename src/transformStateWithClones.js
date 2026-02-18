52484cfc021daaaa796a4218d7d083413a0eb6f1
Banderos14 <anton.shyshenko@gmail.com>
2026-02-18 13:37:51 +0100

    feat: task solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+
+  let copyState = { ...state };
+
+  for (const action of actions) {
+    const { type, extraData, keysToRemove } = action;
+
+    if (type === 'addProperties') {
+      copyState = { ...copyState, ...extraData };
+    }
+
+    if (type === 'removeProperties') {
+      const nextState = { ...copyState };
+
+      for (const key of keysToRemove) {
+        delete nextState[key];
+      }
+      copyState = nextState;
+    }
+
+    if (type === 'clear') {
+      copyState = {};
+    }
+
+    history.push({ ...copyState });
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
